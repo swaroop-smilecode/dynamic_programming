db42413a3af2e9fc01e826341f035d6619e78c4a
swaroop <swaroopiit@gmail.com>
2024-10-23 12:41:55 +0530

    Solved some more problems

--- Problems/p04_min_change/p04_min_change.py
+++ Problems/p04_min_change/p04_min_change.py
@@ -1,10 +1,26 @@
 def min_change(amount, coins):
+    ans = _min_change(amount, coins, {})
+    if ans == float('inf'):
+        return -1
+    else:
+        return ans
+
+def _min_change(amount, coins, memo):
+    if amount in memo:
+        return memo[amount]
+    
+    if amount < 0:
+        return float("inf")
+
     if amount == 0:
         return 0
     
+    min_coins = float("inf")
     for coin in coins:
-        if min_change(amount-coin, coins):
-            return True
+        num_of_coins = 1 + _min_change(amount-coin, coins, memo)
+        min_coins = min(min_coins, num_of_coins)
 
+    memo[amount] = min_coins
+    return min_coins
 
 print(min_change(8, [1, 5, 4, 12])) # -> 2, because 4+4 is the minimum coins possible
