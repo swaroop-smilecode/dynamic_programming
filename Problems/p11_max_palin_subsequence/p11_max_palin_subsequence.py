d48167a3c36da2f45748746cad5eb0f55db3eeb9
swaroop <swaroopiit@gmail.com>
2024-10-26 15:42:12 +0530

    Solved some more problems

--- Problems/p11_max_palin_subsequence/p11_max_palin_subsequence.py
+++ Problems/p11_max_palin_subsequence/p11_max_palin_subsequence.py
@@ -1,7 +1,24 @@
 def max_palin_subsequence(string):
-  return max_palin_subsequence_helper(string,0,len(string)-1,{})
+  return _max_palin_subsequence(string,0,len(string)-1, {})
 
-def max_palin_subsequence_helper(string,i,j,memo):
+def _max_palin_subsequence(string,i,j, memo):
+    key = (i, j)
+    if key in memo:
+        return memo[key]
+    
+    if i > j:
+        return 0
+    
+    if i == j:
+        return 1
 
-
+    if string[i] == string[j]:
+        memo[key] = 2 + _max_palin_subsequence(string, i + 1, j - 1, memo)
+        return memo[key]
+    else:
+        memo[key] = max(
+                        _max_palin_subsequence(string, i + 1, j, memo),
+                        _max_palin_subsequence(string, i, j - 1, memo))
+        return memo[key]
+    
 print(max_palin_subsequence("luwxult")) # -> 5
